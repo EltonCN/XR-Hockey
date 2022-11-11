cb150c2f595b26be503e954b9275ccfc03354032
Elton Cardoso do Nascimento <43186596+EltonCN@users.noreply.github.com>
2022-11-11 20:55:59 -0300

    Tentando debugar o eixo incorreto

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -11,7 +11,9 @@ public class Defender : MonoBehaviour
 
     void Start()
     {
-        localRight = transform.parent.forward;
+        localRight = transform.forward;
+
+        print(localRight);
         camera = Camera.main;
         rb = GetComponent<Rigidbody>();
     }
@@ -20,6 +22,8 @@ public class Defender : MonoBehaviour
     {
         Vector3 this2camera = camera.transform.position - this.transform.position; //This to camera position vector
         Vector3 movement = Vector3.Dot(this2camera, localRight) * localRight; //Previous vector projected to right direction
+
+        print(this2camera.ToString()+ " | " + movement.ToString());
 
         Vector3 targetPosition = this.transform.position + movement; //Target position to match the camera in the right direction
 
