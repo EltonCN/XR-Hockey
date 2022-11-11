c7755fd04251db0f8a640039419fc09aff94c905
Elton Cardoso do Nascimento <43186596+EltonCN@users.noreply.github.com>
2022-11-11 20:30:18 -0300

    Defender segue a câmera

---
+++ Assets/Scripts/Defender.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Defender : MonoBehaviour
+{
+    new Camera camera;
+    Vector3 localRight;
+    Rigidbody rb;
+
+    void Start()
+    {
+        localRight = transform.parent.forward;
+        camera = Camera.main;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 this2camera = camera.transform.position - this.transform.position; //This to camera position vector
+        Vector3 movement = Vector3.Dot(this2camera, localRight) * localRight; //Previous vector projected to right direction
+
+        Vector3 targetPosition = this.transform.position + movement; //Target position to match the camera in the right direction
+
+        Ray ray = new Ray(transform.position, movement);
+        RaycastHit hit;
+
+       if(!rb.SweepTest(movement, out hit, movement.magnitude)) //Check if will not collide in anything
+        {
+            rb.MovePosition(targetPosition);
+        }
+        
+    }
+}
